ee511c75effb3e20bb3dd55646e87dbeabd2ecc2
James Frowen <jamesfrowendev@gmail.com>
2020-07-20 23:54:42 +0100

    fix isServer false in OnDestroy

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -1228,7 +1228,12 @@ namespace Mirror
             {
                 UnityEngine.Object.Destroy(identity.gameObject);
             }
-            identity.Reset();
+            // if we are destroying the server object we don't need to reset the identity
+            // reseting it will cause isClient/isServer to be false in the OnDestroy call
+            else
+            {
+                identity.Reset();
+            }
         }
 
         /// <summary>
