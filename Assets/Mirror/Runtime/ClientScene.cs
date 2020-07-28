c5a8b78f84d885d79d42578de226b46f99dad124
James Frowen <jamesfrowendev@gmail.com>
2020-07-28 13:11:30 +0100

    making ApplySpawnPayload internal for tests

--- Assets/Mirror/Runtime/ClientScene.cs
+++ Assets/Mirror/Runtime/ClientScene.cs
@@ -737,7 +737,7 @@ namespace Mirror
             }
         }
 
-        static void ApplySpawnPayload(NetworkIdentity identity, SpawnMessage msg)
+        internal static void ApplySpawnPayload(NetworkIdentity identity, SpawnMessage msg)
         {
             if (msg.assetId != Guid.Empty)
                 identity.assetId = msg.assetId;
